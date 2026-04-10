13c545a1cdee8daf6aeb42a30a6ee2450f71e0c8
uttkarsh <uttkarshshrivastav30@gmail.com>
2026-04-10 06:20:15 +0530

    updating readme

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
   publicDir: '../public',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        player: resolve(__dirname, 'src/player.html')
+      }
+    }
   },
   server: {
     open: true
