e2ea3819192ffb4345c047809762dcbae04b29d6
uttkarsh <uttkarshshrivastav30@gmail.com>
2026-04-10 06:13:15 +0530

    Fix: Configure build output directory for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   server: {
     open: true
   }
